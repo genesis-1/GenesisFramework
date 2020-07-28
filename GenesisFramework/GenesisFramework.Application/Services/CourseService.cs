4c0b5fa7a03cc097fd05478350abbb86bce95aeb
patrick ntwari <patrick_ntwari@yahoo.com>
2020-07-28 15:31:48 +0200

    PNT: Added Course Service Create Functionality, Updated CourseViewModel.

--- GenesisFramework/GenesisFramework.Application/Services/CourseService.cs
+++ GenesisFramework/GenesisFramework.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using GenesisFramework.Application.Interfaces;
 using GenesisFramework.Application.ViewModels;
+using GenesisFramework.Domain.Commands;
+using GenesisFramework.Domain.Core.Bus;
 using GenesisFramework.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,11 +12,24 @@ namespace GenesisFramework.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name,
+                courseViewModel.Description,
+                courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel() { 
